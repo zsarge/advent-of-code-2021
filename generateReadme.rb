8f08a4f7ba9e4e2e4c13a2c4e2e58c8b3d184178
zsarge <zackarywsargent+github@gmail.com>
2021-12-02 15:46:07 -0500

    change readme design

--- generateReadme.rb
+++ generateReadme.rb
@@ -154,21 +154,19 @@ content = "\
 -->
 #{FULL_TABLE}
 
-<br>
-
-## Goal
+## Code Quality
 
 These solutions emphasize readability.
 
 While readability is a subjective metric,
-it is my goal that these solutions don't
+I really just don't want these solutions to
 look like code golf.
 
 ## Miscellaneous
 
 I have some scripts set up to make working on problems a smoother experience:
 
- - [`addSolution.rb`](addSolution.rb) takes a problem number, creates the associated folder and file, and copies the problem's prompt from the website.
+ - [`addSolution.rb`](addSolution.rb) takes a problem number, creates the associated folder and solution template, and creates an input file to paste into.
  - [`generateReadme.rb`](generateReadme.rb) generates this readme based on the files in the solutions folder.
 
 Some of the scripts were reused from [my Project Euler solutions](https://github.com/zsarge/ProjectEuler).
