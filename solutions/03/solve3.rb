e8f25889eeb7f74fb25756e605eb58e67957a93e
zsarge <zackarywsargent+github@gmail.com>
2021-12-03 01:02:36 -0500

    remove helper methods

--- solutions/03/solve3.rb
+++ solutions/03/solve3.rb
@@ -20,28 +20,18 @@ end
 
 def part_2
   def get_most_common array
-    def helper foo
-      ones = foo.count { _1 == 1 }
-      zeros = foo.size - ones
-      if ones >= zeros
-        1
-      else
-        0
-      end
+    array.transpose.map(&:reverse).map do |arr| 
+      ones = arr.count { _1 == 1 }
+      zeros = arr.size - ones
+      ones >= zeros ? 1 : 0
     end
-    array.transpose.map(&:reverse).map {|arr| helper arr }
   end
   def get_least_common array
-    def helper foo
-      ones = foo.count { _1 == 1 }
-      zeros = foo.size - ones
-      if ones < zeros
-        1
-      else
-        0
-      end
+    array.transpose.map(&:reverse).map do |arr|
+      ones = arr.count { _1 == 1 }
+      zeros = arr.size - ones
+      ones < zeros ? 1 : 0
     end
-    array.transpose.map(&:reverse).map {|arr| helper arr }
   end
 
   oxygen_arr = INPUTS
@@ -49,12 +39,8 @@ def part_2
   (0...oxygen_arr[0].size).each do |index|
     most_common = get_most_common oxygen_arr
 
-    # p "most common"
-    # p most_common
-
     oxygen_arr = oxygen_arr.filter {|arr| arr[index] == most_common[index]}
 
-    p oxygen_arr.size
     if oxygen_arr.size == 1
       break
     end
@@ -65,22 +51,15 @@ def part_2
   (0...co2_arr[0].size).each do |index|
     least_common = get_least_common co2_arr
 
-    # p "least common"
-    # p least_common
-
     co2_arr = co2_arr.filter {|arr| arr[index] == least_common[index]}
 
-    p co2_arr.size
     if co2_arr.size == 1
       break
     end
   end
 
-  p oxygen_arr
-  p co2_arr
   co2_arr.join.to_i(2) * oxygen_arr.join.to_i(2)
 end
 
-# p INPUTS.zip INPUTS.map { most_common_bit _1 }
 # p part_1
 p part_2
