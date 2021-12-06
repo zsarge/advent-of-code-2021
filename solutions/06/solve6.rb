5b7b574975e6907a67072672bf333e18682586e1
zsarge <zackarywsargent+github@gmail.com>
2021-12-06 08:24:07 -0500

    choose a better name

--- solutions/06/solve6.rb
+++ solutions/06/solve6.rb
@@ -7,18 +7,18 @@
 INPUTS = File.read("input6.txt").chomp.split(",").map(&:to_i)
 
 [80, 256].each do |days|
-  count = INPUTS.reduce(Array.new(9 ,0)) do |acc, ele| 
+  cycle = INPUTS.reduce(Array.new(9 ,0)) do |acc, ele| 
 	acc[ele] += 1
 	acc 
   end
 
   days.times do
 	# increase the number on the 7th day by the amount on the 1st day
-	count[7] += count[0]
+	cycle[7] += cycle[0]
 	# move the array along
-	count.rotate!(1)
+	cycle.rotate!(1)
   end
 
-  p count.sum
+  p cycle.sum
 end
 
