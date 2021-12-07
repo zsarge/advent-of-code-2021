4a74aad903fddb36623cbb753a1662614f75260d
zsarge <zackarywsargent+github@gmail.com>
2021-12-07 01:08:30 -0500

    clean up solutions

--- solutions/07/solve7.rb
+++ solutions/07/solve7.rb
@@ -8,9 +8,7 @@ FILE_NAME = "input7.txt"
 # FILE_NAME = "test.txt"
 INPUTS = File.read(FILE_NAME).scan(/\d+/).map(&:to_i)
 
-# def get_cheapest_pos positions
-# end
-
+# O(n^2)
 def part_1
   # returns the amount of fuel it takes to
   # move all crabs to that point
@@ -18,41 +16,26 @@ def part_1
     INPUTS.map{ |input| (input - point).abs }.sum
   end
 
-  final = INPUTS.map { |input|
-    [input, consider(input)]
-  }.min_by{ |(_, total_fuel)| total_fuel }.last
+  INPUTS.map { |input|
+    consider(input)
+  }.min
 end
 
-# p part_1
+# O(n^2)
+def part_2
+  # returns the amount of fuel it takes to
+  # move all crabs to that point
+  def consider point
+    INPUTS.map { |input|
+      n = (input - point).abs 
+      (n*(n + 1)) / 2
+    }.sum
+  end
 
-# returns the amount of fuel it takes to
-# move all crabs to that point
-# O(n)
-def consider point
-  res = INPUTS.map { |input|
-    n = (input - point).abs 
-    (n*(n + 1)) / 2
-  }
-  # p res
-  res.sum
+  (INPUTS.min..INPUTS.max).map { |input|
+    consider(input)
+  }.min
 end
 
-p consider 5
-
-# p INPUTS.sort_desc
-
-final = (INPUTS.min..INPUTS.max).map { |input|
-  [input, consider(input)]
-}.min_by {|(_, total_fuel)| total_fuel }
-
-p final
-p :final
-p final.last
-
-# p INPUTS
-# p INPUTS.map{ _1 - 2 }
-# p INPUTS.map{ (_1 - 2).abs }.sum
-
-# p INPUTS.map{ (_1 - 2).abs }
-
-
+p part_1
+p part_2
