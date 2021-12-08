cf44aa8f6ba270f8da7360127642669effacf665
zsarge <zackarywsargent+github@gmail.com>
2021-12-07 21:25:42 -0500

    remove code golf

--- solutions/07/solve7.rb
+++ solutions/07/solve7.rb
@@ -39,7 +39,3 @@ end
 p part_1
 p part_2
 
-# P.S. here is this solution code golfed:
-=begin
-i=File.read("input7.txt").scan(/\d+/).map(&:to_i);[->p{i.map{(_1-p).abs}.sum},->p{i.map{n=(_1-p).abs;(n*(n+1))/2}.sum}].each{|c|p (i.min..i.max).map{c.call _1}.min}
-=end
